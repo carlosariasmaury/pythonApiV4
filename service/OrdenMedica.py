735c27a100dcd681a7944ed355442cc425a91d4a
carlosariasmaury <47978223+carlosariasmaury@users.noreply.github.com>
2020-10-23 09:01:23 -0500

    Update OrdenMedica.py

--- service/OrdenMedica.py
+++ service/OrdenMedica.py
@@ -1,83 +1,34 @@
-
-Skip to content
-Pull requests
-Issues
-Marketplace
-Explore
-@carlosariasmaury
-Learn Git and GitHub without any code!
-
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-carlosariasmaury /
-pythonApiV4
-
-1
-0
-
-    0
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-
-    Settings
-
-pythonApiV4/service/client.py /
-alejandro triana Initial commit
-Latest commit 0299c23 3 days ago
-History
-0 contributors
-34 lines (26 sloc) 1021 Bytes
-""" Class representing a client
+""" Class representing a Orden Medica
     """
-class Client():
+class OrdenMedica():
 
-    def __init__(self, name, last_name, doc_id):
-        self.name = name
-        self.last_name = last_name
-        self.doc_id = doc_id
-        self.preexistence = []
+    def __init__(self, identifier, status, category):
+        self.identifier = identifier
+        self.status = status
+        self.category = category
+        self.orderDetail = []
 
-    def add_preexistence(self, nPreexistence):
-        self.preexistence.append(nPreexistence)
-        return len(self.preexistence) - 1
+    def add_orderDetail(self, orderDetail):
+        self.orderDetail.append(orderDetail)
+        return len(self.orderDetail) - 1
 
-    def get_preexistence(self, pIndex):
-        if pIndex >= len(self.preexistence):
-            return 'There is no such preexistence'
+    def get_orderDetail(self, pIndex):
+        if pIndex >= len(self.orderDetail):
+            return 'There is no such orderDetail'
         else:
-            return self.preexistence[pIndex]
+            return self.orderDetail[pIndex]
 
-    def get_all_preexistence(self):
+    def get_all_orderDetail(self):
         return self.preexistence
 
-    def remove_preexistence(self, n_preexistence):
-        self.preexistence.pop(n_preexistence)
-        return len(self.preexistence) - 1
+    def remove_orderDetail(self, n_orderDetail):
+        self.orderDetail.pop(n_orderDetail)
+        return len(self.orderDetail) - 1
 
-    def get_formatted_name(self):
-        return self.name + ' ' + self.last_name
+    def get_formatted_OrderStatus(self):
+        return self.identifier + ' ' + self.status
 
 
 if __name__ == '__main__':
-    client_instance = Client('uno', 'dos', '113565')
-    print('User Abbas has been added with id ', client_instance.get_formatted_name())
-
-    © 2020 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
+    client_instance = Client('id1', 'Aprobado', 'Med')
+    print('El Estado de la OM es  ', client_instance.get_formatted_OrderStatus())
