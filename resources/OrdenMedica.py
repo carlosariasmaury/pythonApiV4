8f5ca39ca99d0c9f7c239e9d8c2ac81815d4851f
cdavidquiroga <70729863+cdavidquiroga@users.noreply.github.com>
2020-10-29 08:34:38 -0500

    Create OrdenMedica.py

---
+++ resources/OrdenMedica.py
@@ -0,0 +1,37 @@
+from flask import Response, request
+from database.models import OrdenMedica
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import Resource
+
+
+class OrdenMedicaApi(Resource):
+    def get(self):
+        OrdenesMedicas = OrdenMedica.objects().to_json()
+        return Response(OrdenesMedicas, mimetype="application/json", status=200)
+
+    @jwt_required
+    def post(self):
+        body = request.get_json()
+        OrdenMedica = OrdenMedica(**body).save()
+        id = OrdenMedica.id
+        return {'id': str(id)}, 200
+
+
+class OrdenMedicaApi(Resource):
+    @jwt_required
+    def put(self, id):
+        body = request.get_json()
+        OrdenMedica.objects.get(id=id).update(**body)
+        return '', 200
+
+    @jwt_required
+    def delete(self, id):
+        OrdenMedica = OrdenMedica.objects.get(id=id).delete()
+        return '', 200
+
+    def get(self, id):
+        try:
+            OrdenMedica = OrdenMedica.objects.get(id=id).to_json()
+            return Response(OrdenMedica, mimetype="application/json", status=200)
+        except Exception as error:
+            return Response(error, status=400, mimetype='application/json')
