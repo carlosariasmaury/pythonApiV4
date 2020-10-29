fa1be99481f7e528fd3708f34021189d9feb4683
cdavidquiroga <70729863+cdavidquiroga@users.noreply.github.com>
2020-10-29 08:22:08 -0500

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,8 +1,8 @@
 from .db import db
 
-class Client(db.Document):
-    doc_id = db.StringField(required=True, unique=True)
-    name = db.StringField(required=True)
-    last_name = db.StringField(required=True)
-    preexistence = db.ListField(db.StringField(), required=True)
+class OrdenMedica():
+    identifier  = db.StringField(required=True, unique=True)
+    status  = db.StringField(required=True)
+    category  = db.StringField(required=True)
+    orderDetail  = db.ListField(db.StringField(), required=True)
 
