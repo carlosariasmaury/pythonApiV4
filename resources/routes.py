4cb4896dfa95c780c26f08d461980e18411239b7
carlosariasmaury <47978223+carlosariasmaury@users.noreply.github.com>
2020-10-29 08:40:43 -0500

    Update routes.py

--- resources/routes.py
+++ resources/routes.py
@@ -1,7 +1,10 @@
 from .client import ClientsApi, ClientApi
+from .OrdenMedica import OrdenesMedicasApi, OrdenMedicaApi
 
 
 def initialize_routes(api):
     api.add_resource(ClientsApi, '/api/clients')
     api.add_resource(ClientApi, '/api/client/<id>')
+    api.add_resource(OrdenesMedicasApi, '/api/OrdenesMedicas')
+    api.add_resource(OrdenMedicaApi, '/api/OrdenMedica/<id>')
 
